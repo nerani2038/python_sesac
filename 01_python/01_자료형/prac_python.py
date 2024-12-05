8b11617bdfb8c661f2e0ca47d610a45ebc6aaddd
Hyeran Jeon <nerani2038@naver.com>
2024-12-05 11:44:16 +0900

    241204_python_03 변수명 생성

---
+++ 01_python/01_자료형/prac_python.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+print(type(1))
+2+4
+print(type({'a':30}))
+print('------------------------')
+print(type([]))
+print(type({}))
+print(type(()))
+print('------------------------')
+x = {'a': 10}
+print(x['a'])
+print('------------------------')
+print(type(False))
+print(type(1 == 2))
