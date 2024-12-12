4a1bd34b09ed256518aef8395313f2b6ac31b87e
Hyeran Jeon <nerani2038@naver.com>
2024-12-12 16:26:18 +0900

    241212_python_07 패키지

---
+++ 01_python/05_패키지/shapes/__init__.py
@@ -0,0 +1,1 @@
+from shapes import volume, area2
---
+++ 01_python/05_패키지/shapes/area2.py
@@ -0,0 +1,11 @@
+PI = 3.14 
+
+print(f'area 모듈 이름: {__name__}')
+
+PI = 3.14 # 상수는 대문자로 변수 지정 
+
+def circle(radius):
+    return radius * radius * PI
+
+def square(length):
+    return length * length
