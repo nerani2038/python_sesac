2a491f32552b5c88400ab998cf0911dd2a2e64d6
Hyeran Jeon <nerani2038@naver.com>
2024-12-23 17:47:20 +0900

    241223_web_crawling_07 streamlit

--- 03_시각화/my_streamlit/00_hello.py
+++ 03_시각화/my_streamlit/00_hello.py
@@ -3,3 +3,16 @@ import pandas as pd
 
 # 웹 제목 설정
 st.title('My Streamlit App(First)')
+
+# 텍스트 출력
+st.write('안녕하세요! streamlit입니다!')
+
+# 데이터 프레임 만들기
+data = {'name': ['Alice', 'Bob', 'Charlie'], 'age': [30, 25, 22]} 
+df = pd.DataFrame(data=data)
+
+# 데이터 츠레임 출력
+st.dataframe(df)
+
+# 막대 그래프 생성
+st.bar_chart(df['age'])
